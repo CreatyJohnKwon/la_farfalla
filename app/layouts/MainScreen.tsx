13e2147ad721e0f6229ef0218a5785e1cf7205f2
woc-dev-team <wordofchurch22@gmail.com>
2025-04-02 01:16:38 +0900

    ♻️ refactor: 코드 리팩토링
    * MainScreeen.tsx 수정
    
    resolves: #12

--- app/layouts/MainScreen.tsx
+++ app/layouts/MainScreen.tsx
@@ -5,10 +5,6 @@ import Chillguy from "@/public/chill.png"
 import Link from "next/link";
 
 const MainScreen = () => {
-    // const 침착맨유튜브 = () => {
-    //     confirm("침며드시겠습니까?") ? window.open("https://www.youtube.com/shorts/iplAx5EK_KU", "침착맨유튜브") : alert("아쉽네요..")
-    // }
-
     return (
         <div className="flex flex-col items-center justify-center min-h-screen w-screen overflow-hidden">
             <div className="relative w-1/2 h-1/2 font-serif text-center text-9xl text-black -mt-72">
