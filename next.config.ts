7857d66ba0d97615339fcca36b75e0adad52a2ea
woc-dev-team <wordofchurch22@gmail.com>
2025-04-03 02:46:45 +0900

    👷 build: package 추가
    * config 이미지 url 허용 설정 추가
    
    resolves: #17

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true, // React Strict Mode 활성화
+  images: {
+    domains: ["phinf.pstatic.net", "k.kakaocdn.net"], // 외부 이미지 도메인 추가
+  },
   async headers() {
     return [
       {
-        source: '/api/:path*',  // 경로 설정
+        source: '/api/:path*', // 경로 설정
         headers: [
           { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
