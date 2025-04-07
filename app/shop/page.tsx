4e8f93d693f2fe4e5c621fd5ac9738ec0e256ad6
권준우 <gwonjun-u@192.168.nate.com>
2025-04-07 19:17:53 +0900

    ✨ feat: package 및 MongoDB 기술 추가
    *  MongoDB 패키지 및 기초 틀 추가
    
    resolves: #24

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,9 +1,14 @@
-const Shop = () => {
+import { connectDB } from "@/app/utils/database";
+
+const Shop = async () => {
+    const client = await connectDB;
+    const db = client.db("forum");
+    let result = await db.collection("post").find({}).toArray();
+    console.log(result);
+
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <div className="items-start">
-                <h1 className="text-7xl font-serif mb-20">Shop Page</h1>
-            </div>
+            <div></div>
         </div>
     )
 }
