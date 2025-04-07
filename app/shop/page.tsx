1f4c95a0a984718ff6bc14f350e1c9f906b3c6ae
woc-dev-team <wordofchurch22@gmail.com>
2025-04-08 01:57:49 +0900

    🐛 hotfix: bug 를 일으키는 useDatas 제거
    * 서버에서 사용할 수 없는 커스텀 훅 제거
    
    resolves: #23

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,20 +1,21 @@
 import { connectDB } from "@/app/utils/database";
 
 const Shop = async () => {
+
     const client = await connectDB;
     const db = client.db("forum");
     let result = await db.collection("post").find({}).toArray();
-    console.log(result);
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-<<<<<<< HEAD
-            <div className="items-start grid grid-cols">
-                
-            </div>
-=======
-            <div></div>
->>>>>>> 4e8f93d693f2fe4e5c621fd5ac9738ec0e256ad6
+            <ul>
+                {result.map((post) => (
+                    <li key={`${post._id}`}>
+                        <h1>{post.title}</h1>
+                        <p>{post.content}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
