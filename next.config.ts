31313a9f5cabdc83d31b3604526c560cbfc6f937
woc-dev-team <wordofchurch22@gmail.com>
2025-04-11 06:52:46 +0900

    ♻️ refactor: next.config.ts에서 이미지 도메인 설정을 remotePatterns로 변경 및 prettier.config.cjs에서 코드 정리 🗑️ chore: 사용하지 않는 SVG 파일 삭제 ✨ feat: tsconfig.json에서 paths 설정을 app/*로 수정
    resolves: #32

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,37 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true, // React Strict Mode 활성화
   images: {
-    domains: ["phinf.pstatic.net", "k.kakaocdn.net", "pub-29feff62c6da44ea8503e0dc13db4217.r2.dev"], // 외부 이미지 도메인 추가
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "phinf.pstatic.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "k.kakaocdn.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-29feff62c6da44ea8503e0dc13db4217.r2.dev",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
   async headers() {
     return [
       {
-        source: '/api/:path*', // 경로 설정
+        source: "/api/:path*", // 경로 설정
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-Custom-Header' },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-Custom-Header" },
         ],
       },
     ];
