61f165ccc197bcd2be8e905150571298160ef325
woc-dev-team <wordofchurch22@gmail.com>
2025-04-11 07:11:34 +0900

    🚑 hotfix: Shop 버그 수정
    * Shop 페이지에 Suspense 및 로딩 이미지 추가
    
    resolves: #35

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,6 +1,9 @@
 import { connectDB } from "@/utils/context/database";
 import { Post } from "@/utils/types/interfaces";
 import ShopClient from "@/utils/component/Shop/ShopClient";
+import { Suspense } from "react";
+import Image from "next/image";
+import LoadingIcon from "../../public/chill.png";
 
 const Shop = async () => {
     const db = (await connectDB).db("forum");
@@ -14,7 +17,32 @@ const Shop = async () => {
         _id: post._id.toString(),
     })) as Post[];
 
-    return <ShopClient posts={cleanedResult} />;
+    return (
+        <div>
+            {/* Client Component는 Suspense로 감싸야 함 */}
+            <Suspense
+                fallback={
+                    <div className="flex h-screen w-screen flex-col items-center justify-center">
+                        <Image
+                            src={LoadingIcon}
+                            alt="loading_img"
+                            width={500}
+                            height={500}
+                            className="top-50 fixed z-0 mb-10 blur-lg"
+                            priority
+                        />
+                        <div className="z-10 h-auto">
+                            <span className="font-brand h-full w-full justify-self-center text-3xl c_md:text-6xl">
+                                Loading...
+                            </span>
+                        </div>
+                    </div>
+                }
+            >
+                <ShopClient posts={JSON.parse(JSON.stringify(result))} />
+            </Suspense>
+        </div>
+    );
 };
 
 export default Shop;
