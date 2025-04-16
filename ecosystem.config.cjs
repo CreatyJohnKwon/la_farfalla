df082dca1a05f26632c197d910ce5c9a80651506
woc-dev-team <wordofchurch22@gmail.com>
2025-04-16 21:09:04 +0900

    ✨ feat: CI/CD 에러 수정
    * docker-compose 빌더 명령어 추가
    
    resolves: #47

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,13 @@
 module.exports = {
-  apps: [
-    {
-      name: "laf-server",
-      cwd: "/home/ec2-user/la_farfalla", // ✅ 프로젝트 경로 지정
-      script: "node_modules/next/dist/bin/next",
-      args: "start",
-      env: {
-        NODE_ENV: "production",
-      },
-    },
-  ],
+    apps: [
+        {
+            name: "laf-server",
+            cwd: "/home/ec2-user/la_farfalla",
+            script: "node_modules/next/dist/bin/next",
+            args: "start",
+            env: {
+                NODE_ENV: "production",
+            },
+        },
+    ],
 };
-
