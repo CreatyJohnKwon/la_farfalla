01e318fa874f654032d97f8727093f51b7f4e40f
Junwoo Kwon <106053673+CreatyJohnKwon@users.noreply.github.com>
2025-04-22 19:25:30 +0900

    Create Footer.tsx

---
+++ app/src/widgets/footer/Footer.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+const Footer = () => {
+    return (
+        <div className="font-brand-thin fixed bottom-0 w-screen text-center text-[10px] text-black sm:text-sm">
+            <p className="-mb-1">business license : lafarfalla</p>
+            <p className="-mb-1">
+                address: 15-5, Jangwol-ro 2-gil, Seongbuk-gu SEOUL, 02773, KOREA
+            </p>
+            <p className="mb-2">mon to fri 9:00 - 19:00</p>
+        </div>
+    );
+};
+
+export default Footer;
