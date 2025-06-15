69fe8996fc0a77cccb02835c9024658930584f8d
woc-dev-team <wordofchurch22@gmail.com>
2025-06-15 23:41:39 +0900

    🚨  hotfix: 에러 수정
    - seasonId 를 ID 타입으로 하게되면 기존에 있는 데이터에 중복오류가 생기기에 string 타입으로 변경

--- app/src/entities/models/Product.ts
+++ app/src/entities/models/Product.ts
@@ -39,7 +39,7 @@ const productSchema = new mongoose.Schema(
             required: true,
         },
         seasonId: {
-            type: mongoose.Types.ObjectId,
+            type: String,
             required: true,
         },
         size: {
