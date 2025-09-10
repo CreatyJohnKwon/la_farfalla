49d222753d982c9e6b1a53b0f2c6410b1136aa2f
CreatyJohnKwon <vmfodzl125@naver.com>
2025-09-11 01:15:33 +0900

    🚨 hotfix: 에러 수정
    - navigation type error
    Season -> Category type 적용

--- app/src/entities/type/navigation.ts
+++ app/src/entities/type/navigation.ts
@@ -1,7 +1,7 @@
-import { Season } from "@src/entities/type/interfaces";
+import { Category } from "@src/entities/type/interfaces";
 
 interface NavbarClientProps {
-    category: Season[]; // 이제 props로 데이터를 받습니다.
+    category: Category[]; // 이제 props로 데이터를 받습니다.
 }
 
 export type { NavbarClientProps };
