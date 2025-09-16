c23d854aeb08758d8c6bebca84015fc78a1ef14a
CreatyJohnKwon <vmfodzl125@naver.com>
2025-09-16 20:26:57 +0900

    ♻️ refactor: 프로필 메뉴 ui 수정

--- app/src/widgets/navbar/ProfileNav.tsx
+++ app/src/widgets/navbar/ProfileNav.tsx
@@ -6,21 +6,21 @@ import { redirect } from "next/navigation";
 const ProfileNavbar = ({ id }: { id: string }) => {
     return (
         <nav className="absolute top-12 h-full w-full items-center sm:items-start transition-all duration-300 ease-in-out sm:static sm:top-20 sm:col-span-2 flex flex-col">
-            <div className="font-amstel flex w-[90vw] flex-row justify-between sm:justify-evenly pt-[4vh] text-sm transition-all duration-300 ease-in-out sm:flex-col sm:ps-8 sm:pt-36 sm:text-base md:ps-10 md:text-xl">
+            <ul className="font-amstel flex w-[90vw] flex-row justify-between sm:justify-evenly pt-[4vh] text-sm transition-all duration-300 ease-in-out sm:flex-col sm:ps-8 sm:pt-36 sm:text-base md:ps-10 md:text-xl">
                 {profileNavData.map((navList, index) => (
-                    <div
+                    <li
                         key={`profile_nav_${index}`}
                         className="p-0 sm:p-5"
                     >
                         <button
-                            className={`z-40 text-black ${navList.id.toLowerCase() === id ? "underline decoration-1" : ""}`}
+                            className={`z-40 ${navList.id.toLowerCase() === id ? "text-black/50 decoration-1" : "text-black"}`}
                             onClick={() => redirect(navList.id.toLowerCase())}
                         >
                             {navList.id}
                         </button>
-                    </div>
+                    </li>
                 ))}
-            </div>
+            </ul>
         </nav>
     );
 };
